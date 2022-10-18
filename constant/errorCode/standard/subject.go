dd49b0b072e83de05aafad737113b29f37695894
Tanapong <tanapong@robowealth.co.th>
2022-10-18 10:16:00 +0700

    update error code

--- constant/errorCode/standard/subject.go
+++ constant/errorCode/standard/subject.go
@@ -4,8 +4,20 @@ func (e value) Subject() string {
 	switch e {
 	case BAD_REQUEST:
 		return "BAD_REQUEST"
-	case INTERNAL:
-		return "INTERNAL"
+	case UNAUTHORIZED:
+		return "UNAUTHORIZED"
+	case FORBIDDEN:
+		return "FORBIDDEN"
+	case NOT_FOUND:
+		return "NOT_FOUND"
+	case UNSUPPORTED_MEDIA_TYPE:
+		return "UNSUPPORTED_MEDIA_TYPE"
+	case UNPROCESSABLE_ENTITY:
+		return "UNPROCESSABLE_ENTITY"
+	case INTERNAL_SERVER_ERROR:
+		return "INTERNAL_SERVER_ERROR"
+	case GATEWAY_TIMEOUT:
+		return "GATEWAY_TIMEOUT"
 	default:
 		return ""
 	}
