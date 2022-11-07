325e9f59a64a6049fc26fbce22fbeb57fcdc94eb
yarnin086 <yarnin@robowealth.co.th>
2022-11-07 20:25:41 +0700

    Update global.go

--- global.go
+++ global.go
@@ -1,74 +1,38 @@
 package stdlog
 
 import (
-	"github.com/rs/zerolog"
-	"time"
+	"os"
+
+	"golang.org/x/exp/slog"
 )
 
-func SetGlobalTimestamp() {
-	logger.With().Timestamp()
+type GlobalSetting struct {
+	level        Level
+	platformName string
 }
 
-func SetGlobalLogLevel(level Level) {
-	var logLv zerolog.Level
+func NewGlobalSetting() *GlobalSetting {
+	return &GlobalSetting{}
+}
 
-	switch level {
-	case TraceLevel:
-		logLv = zerolog.TraceLevel
-	case DebugLevel:
-		logLv = zerolog.DebugLevel
-	case InfoLevel:
-		logLv = zerolog.InfoLevel
-	case WarnLevel:
-		logLv = zerolog.WarnLevel
-	case ErrorLevel:
-		logLv = zerolog.ErrorLevel
-	case FatalLevel:
-		logLv = zerolog.FatalLevel
-	case PanicLevel:
-		logLv = zerolog.PanicLevel
-	case Disabled:
-		logLv = zerolog.Disabled
-	case NoLevel:
-		logLv = zerolog.NoLevel
-	default:
-		logLv = zerolog.NoLevel
+func (gs *GlobalSetting) WithLevel(level Level) *GlobalSetting {
+	gs.level = level
+	logLevel = level
+	return gs
+}
+
+func (gs *GlobalSetting) WithPlatformName(name string) *GlobalSetting {
+	gs.platformName = name
+	return gs
+}
+
+func (gs *GlobalSetting) Build() {
+	var attrs []slog.Attr
+
+	if gs.platformName != "" {
+		attrs = append(attrs, slog.Any(platformName, gs.platformName))
 	}
 
-	zerolog.SetGlobalLevel(logLv)
-}
-
-func SetGlobalPlatformName(name string) {
-	logger.With().Str("platform_name", name)
-}
-
-func SetGlobalTimestampFieldName(key string) {
-	zerolog.TimestampFieldName = key
-}
-
-func SetGlobalTimeFieldFormat(format TimeFormat) {
-	var timeFormat string
-
-	switch format {
-	case TimeFormatUnix:
-		timeFormat = zerolog.TimeFormatUnix
-	case TimeFormatUnixMs:
-		timeFormat = zerolog.TimeFormatUnixMs
-	case TimeFormatUnixMicro:
-		timeFormat = zerolog.TimeFormatUnixMicro
-	case TimeFormatUnixNano:
-		timeFormat = zerolog.TimeFormatUnixNano
-	default:
-		timeFormat = time.RFC3339
-	}
-
-	zerolog.TimeFieldFormat = timeFormat
-}
-
-func SetGlobalLevelFieldName(key string) {
-	zerolog.LevelFieldName = key
-}
-
-func SetGlobalMessageFieldName(key string) {
-	zerolog.MessageFieldName = key
+	h := slog.HandlerOptions{Level: gs.level}.NewJSONHandler(os.Stdout).WithAttrs(attrs)
+	log = slog.New(h)
 }
